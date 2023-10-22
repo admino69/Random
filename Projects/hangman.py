daac53db1f39bbfd8d27b1a55db572828c897d6e
Adam T <TheCrimsonAgents@gmail.com>
2023-10-22 21:18:45 +0200

    commit

--- Projects/hangman.py
+++ Projects/hangman.py
@@ -1,8 +1,27 @@
 #Project: Hangman game
+#Game: Random questions, like guess the fruits. Also show
+#how long the words are. "_ a _ _ _" Maybe with GUI after.
+#maybe hints too, and you will lose a guess if u use a hint
+import random
 
+print(input('''        
+____
+|  |
+|  O    Guess the secret word before he gets hanged!
+| /|\\
+| / \\
 
-'''
-  O
- /|\
- / \
-'''
+    
+Would you like to play hangman? Y/N : '''))
+guesses = 0
+guess_limit = 7
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+def random_word():
+    categories = {"Fruits": ["Apple", "Orange"],
+                "Cars": ["BMW", "Volvo"]}
+    random_key = random.choice(list(categories.keys()))
+    random_value = random.choice(categories[random_key])
+    return random_value
+
+print(random_word())
