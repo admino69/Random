9f67eb867a6185b1a9390c25e37856c4679303b8
Adam T <TheCrimsonAgents@gmail.com>
2023-11-24 23:48:32 +0100

    commit

---
+++ Projects/practical_app.py
@@ -0,0 +1,34 @@
+from tkinter import *
+import customtkinter
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+root = customtkinter.CTk()
+
+root.title("Practical App")
+root.geometry("600x350")
+
+def hello():
+    my_label.configure(text=my_button.cget("text"))
+
+my_button = customtkinter.CTkButton(root,
+    text="Hello World!",
+    command=hello,
+    height=100,
+    width=200,
+    font=("Helvetica", 24),
+    text_color="Black",
+    fg_color=("Red"),
+    hover_color=("green"),
+    corner_radius=50,
+    border_width=10,
+    border_color=("black"),
+    state=NORMAL)
+
+my_button.pack(pady=80)
+
+my_label = customtkinter.CTkLabel(root, text="")
+my_label.pack(pady=20)
+
+root.mainloop()
---
+++ random1.py
@@ -0,0 +1,7 @@
+while True:
+    def plus():
+            score = 0
+            score += 1
+            print(score)
+
+plus()
