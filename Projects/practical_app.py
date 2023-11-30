462585d111379d7ebeeaf1016393ee3e131b5f69
Adam T <TheCrimsonAgents@gmail.com>
2023-11-30 22:21:28 +0100

    commit

--- Projects/practical_app.py
+++ Projects/practical_app.py
@@ -7,28 +7,45 @@ customtkinter.set_default_color_theme("blue")
 root = customtkinter.CTk()
 
 root.title("Practical App")
-root.geometry("600x350")
 
-def hello():
-    my_label.configure(text=my_button.cget("text"))
+root.resizable(False, False)
 
-my_button = customtkinter.CTkButton(root,
-    text="Hello World!",
-    command=hello,
-    height=100,
-    width=200,
-    font=("Helvetica", 24),
-    text_color="Black",
-    fg_color=("Red"),
-    hover_color=("green"),
-    corner_radius=50,
-    border_width=10,
-    border_color=("black"),
-    state=NORMAL)
+height = 400
+width = 550
+x = (root.winfo_screenwidth()//2)-(width//3)
+y = (root.winfo_screenheight()//2)-(height//2)
+root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+#root.geometry("400x400")
 
-my_button.pack(pady=80)
+def submit():
+    my_label.configure(text=f"Hello {my_entry.get()}")
+    my_entry.configure(state=DISABLED)
+
+def clear():
+    my_entry.delete(0, END)
+    my_entry.configure(state=NORMAL)
 
 my_label = customtkinter.CTkLabel(root, text="")
-my_label.pack(pady=20)
+my_label.pack(pady=40)
+
+creat_name_label = customtkinter.CTkLabel(root,
+     text="Created By Adam",
+     font=("",12))
+creat_name_label.place(x=15,y=5)
+
+my_entry = customtkinter.CTkEntry(root, 
+    placeholder_text="Enter Your Name",
+    height=50,
+    width=200,
+    font=("",18),
+    corner_radius=50,
+    fg_color=("","black"))
+my_entry.pack(pady=20)
+
+my_button = customtkinter.CTkButton(root, text="Submit", command=submit)
+my_button.pack(pady=10)
+
+clear_button = customtkinter.CTkButton(root, text="Clear", command=clear)
+clear_button.pack(pady=10)
 
 root.mainloop()
--- random1.py
+++
@@ -1,7 +0,0 @@
-while True:
-    def plus():
-            score = 0
-            score += 1
-            print(score)
-
-plus()
